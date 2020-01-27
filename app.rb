9c136d298c5a0017ea33d3a28172ce8643f1890e
Natalia Wit <natwit95@gmail.com>
2020-01-27 18:35:04 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,8 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get ("/name") {"My name is Natalia"}
+    get ('/hometown') {"My hometown is Queens"}
+    get ('/favorite-song') {"My favorite song is Hello"}
 end
